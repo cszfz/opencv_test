402ff34af7039a3b35e53ad1010ccf76bd42a52a
cszfz <z1612494583@163.com>
2019-01-09 21:17:10 +0800

    2019/01/09

--- code/375.py
+++ code/375.py
@@ -1,3 +1,5 @@
+
+
 #coding=utf-8
 import cv2
 import numpy as np
@@ -10,7 +12,7 @@ dirTrains=[
 		   'D:\\image\\train2lt\\',
 		   ]
 
-dirTest='D:\\image\\lt\\'
+dirTest='D:\\image\\'
 picNamesR=['045.jpg','054.jpg','107.jpg','128.jpg']
 picNamesL=['644.jpg','648.jpg','657.jpg','701.jpg','705.jpg','708.jpg']
 #中心矩数
@@ -21,7 +23,7 @@ min_area=15000
 
 #f_mean=np.mean(f_train, axis=0)
 f_mean=np.loadtxt(dirTrains[0]+"image_train_mean.txt",delimiter=' ')
-picName='644.jpg'
+picName='00000003.TIF'
 
 #读取图片
 img=cv2.imread(dirTest+picName,0)
@@ -68,7 +70,7 @@ fgdModel=np.zeros((1,65),np.float64)
 rect=(3,3,w-6,h-6)
 
 cv2.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
-
+print(cv2.GC_PR_FGD)
 mask2=np.where((mask==2)|(mask==0),0,1).astype('uint8')
 img=img*mask2[:,:,np.newaxis]
 
@@ -83,51 +85,3 @@ ret,thresh = cv2.threshold(img,50,255,0)
 
 
 cv2.imwrite('thresh.jpg',thresh)
-#轮廓检测
-_,contours,hierarchy = cv2.findContours(thresh, 1, 2)
-cnt = contours[0]
-
-#轮廓特征矩
-M = cv2.moments(cnt)
-
-#归一化中心矩
-feature=[M['nu20'],M['nu11'],M['nu02'],M['nu30'],M['nu21'],M['nu12'],M['nu03']]
-print(str(feature))
-
-for dirTrain in dirTrains:
-	#将txt文件中的moments读取到矩阵f中
-	#矩阵F用来作中间运算
-	f_train=np.loadtxt(dirTrain+"image_train_features.txt",delimiter=' ')
-	F=np.empty(f_train.shape,dtype=float)
-
-
-	#与模型库中的所有图片中心矩比较l
-	F=f_train-feature
-
-	#求出曼哈顿距离最小的图片
-	F=np.abs(F)
-	s=np.sum(F,axis=1)
-	index=np.argmin(s)
-	m=np.min(s)
-
-	print(str(f_train[index]))
-	print(m)
-
-	#获得测试图片的预测偏转角度	
-	flag=0
-	#读取图像名字txt文件
-	image_train_f=open(dirTrain+'image_train_list.txt','r')
-	img_name_train=image_train_f.readline()		
-	img_name_train=img_name_train.strip('\n')	
-	while flag<index:
-		flag=flag+1
-		img_name_train=image_train_f.readline()		
-		img_name_train=img_name_train.strip('\n')
-	image_train_f.close()
-
-	result_img=cv2.imread(dirTrain+img_name_train)
-	print(img_name_train)
-
-
-	cv2.imshow('result',result_img)
-	cv2.waitKey(0)
